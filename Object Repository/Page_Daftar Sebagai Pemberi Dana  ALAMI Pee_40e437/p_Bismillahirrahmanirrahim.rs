974c0e0f4482583cae00307fcf9b92d04b471070
oriwi <oriwi@LAPTOP-OWIDIANT>
2022-04-13 09:03:01 +0700

    First

---
+++ Object Repository/Page_Daftar Sebagai Pemberi Dana  ALAMI Pee_40e437/p_Bismillahirrahmanirrahim.rs
@@ -0,0 +1,89 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>p_Bismillahirrahmanirrahim</name>
+   <tag></tag>
+   <elementGuidId>1bf84c4a-d8b4-475d-9886-8577e7ddcb73</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='scrollWrapper']/p</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>#scrollWrapper > p</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>p</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Bismillahirrahmanirrahim</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;scrollWrapper&quot;)/p[1]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='scrollWrapper']/p</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Kebijakan Privasi &amp; Ketentuan Pengguna'])[1]/following::p[2]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Registrasi Pemberi Dana'])[1]/following::p[2]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='DEFINISI'])[1]/preceding::p[4]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='ALAMI:'])[1]/preceding::p[4]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Bismillahirrahmanirrahim']/parent::*</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//form/div/p</value>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Scripts/Coba/Script1649805663500.groovy
@@ -0,0 +1,102 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('https://dev.p2p.alamisharia.co.id/')
+
+WebUI.click(findTestObject('Object Repository/Page_Login  ALAMI Peer-to-Peer Lending Syariah/a_Daftar disini'))
+
+WebUI.click(findTestObject('Object Repository/Page_ALAMI Daftar  ALAMI Peer-to-Peer Lendi_33cbd2/div_Pemberi Dana'))
+
+WebUI.setText(findTestObject('Object Repository/Page_Daftar Sebagai Pemberi Dana  ALAMI Pee_40e437/input_Nama Lengkap_name'), 
+    'Dona')
+int Number = new Random().nextInt((1000 - 1) + 1) + 10
+
+String Counter1 = 'donadonitasatu+' + Number + '+'
+
+String Counter = Counter1 + '@gmail.com'
+
+GlobalVariable.Counter = Counter
+
+WebUI.setText(findTestObject('Object Repository/Page_Daftar Sebagai Pemberi Dana  ALAMI Pee_40e437/input_Email_email', [(GlobalVariable.Counter) : GlobalVariable.Counter]),
+	GlobalVariable.Counter)
+
+int Number1 = new Random().nextInt((999999999 - 10) + 1) + 10
+
+String Phone = '0812678' + Number
+
+GlobalVariable.Phone = Phone
+
+  
+WebUI.setText(findTestObject('Object Repository/Page_Daftar Sebagai Pemberi Dana  ALAMI Pee_40e437/input_No. Handphone_phone',  [(GlobalVariable.Phone) : GlobalVariable.Phone]),
+	GlobalVariable.Phone)
+    
+
+WebUI.setEncryptedText(findTestObject('Object Repository/Page_Daftar Sebagai Pemberi Dana  ALAMI Pee_40e437/input_Password_password'), 
+    'Xq+Grj40a3e0nkYhGIsjqg==')
+
+WebUI.click(findTestObject('Object Repository/Page_Daftar Sebagai Pemberi Dana  ALAMI Pee_40e437/label_Individual'))
+
+WebUI.selectOptionByValue(findTestObject('Object Repository/Page_Daftar Sebagai Pemberi Dana  ALAMI Pee_40e437/select_-- Silahkan Pilih --                _9f24ad'), 
+    '1', true)
+
+WebUI.click(findTestObject('Object Repository/Page_Daftar Sebagai Pemberi Dana  ALAMI Pee_40e437/button_Lanjutkan'))
+
+WebUI.click(findTestObject('Object Repository/Page_Daftar Sebagai Pemberi Dana  ALAMI Pee_40e437/div_Kebijakan Privasi  Ketentuan Pengguna  _ea668f'))
+
+WebUI.click(findTestObject('Object Repository/Page_Daftar Sebagai Pemberi Dana  ALAMI Pee_40e437/p_Bismillahirrahmanirrahim'))
+WebUI.sendKeys(findTestObject(null), Keys.chord(Keys.ALT, Keys.DOWN))
+WebUI.sendKeys(findTestObject(null), Keys.chord(Keys.ALT, Keys.DOWN))
+WebUI.sendKeys(findTestObject(null), Keys.chord(Keys.ALT, Keys.DOWN))
+WebUI.sendKeys(findTestObject(null), Keys.chord(Keys.ALT, Keys.DOWN))
+WebUI.sendKeys(findTestObject(null), Keys.chord(Keys.ALT, Keys.DOWN))
+WebUI.sendKeys(findTestObject(null), Keys.chord(Keys.ALT, Keys.DOWN))
+WebUI.sendKeys(findTestObject(null), Keys.chord(Keys.ALT, Keys.DOWN))
+WebUI.sendKeys(findTestObject(null), Keys.chord(Keys.ALT, Keys.DOWN))
+WebUI.sendKeys(findTestObject(null), Keys.chord(Keys.ALT, Keys.DOWN))
+WebUI.sendKeys(findTestObject(null), Keys.chord(Keys.ALT, Keys.DOWN))
+WebUI.sendKeys(findTestObject(null), Keys.chord(Keys.ALT, Keys.DOWN))
+WebUI.sendKeys(findTestObject(null), Keys.chord(Keys.ALT, Keys.DOWN))
+WebUI.sendKeys(findTestObject(null), Keys.chord(Keys.ALT, Keys.DOWN))
+WebUI.sendKeys(findTestObject(null), Keys.chord(Keys.ALT, Keys.DOWN))
+WebUI.sendKeys(findTestObject(null), Keys.chord(Keys.ALT, Keys.DOWN))
+WebUI.sendKeys(findTestObject(null), Keys.chord(Keys.ALT, Keys.DOWN))
+WebUI.sendKeys(findTestObject(null), Keys.chord(Keys.ALT, Keys.DOWN))
+WebUI.sendKeys(findTestObject(null), Keys.chord(Keys.ALT, Keys.DOWN))
+WebUI.sendKeys(findTestObject(null), Keys.chord(Keys.ALT, Keys.DOWN))
+WebUI.sendKeys(findTestObject(null), Keys.chord(Keys.ALT, Keys.DOWN))
+WebUI.sendKeys(findTestObject(null), Keys.chord(Keys.ALT, Keys.DOWN))
+WebUI.sendKeys(findTestObject(null), Keys.chord(Keys.ALT, Keys.DOWN))
+WebUI.sendKeys(findTestObject(null), Keys.chord(Keys.ALT, Keys.DOWN))
+WebUI.sendKeys(findTestObject(null), Keys.chord(Keys.ALT, Keys.DOWN))
+WebUI.sendKeys(findTestObject(null), Keys.chord(Keys.ALT, Keys.DOWN))
+WebUI.sendKeys(findTestObject(null), Keys.chord(Keys.ALT, Keys.DOWN))
+WebUI.sendKeys(findTestObject(null), Keys.chord(Keys.ALT, Keys.DOWN))
+WebUI.sendKeys(findTestObject(null), Keys.chord(Keys.ALT, Keys.DOWN))
+WebUI.sendKeys(findTestObject(null), Keys.chord(Keys.ALT, Keys.DOWN))
+WebUI.sendKeys(findTestObject(null), Keys.chord(Keys.ALT, Keys.DOWN))
+WebUI.sendKeys(findTestObject(null), Keys.chord(Keys.ALT, Keys.DOWN))
+WebUI.sendKeys(findTestObject(null), Keys.chord(Keys.ALT, Keys.DOWN))
+WebUI.sendKeys(findTestObject(null), Keys.chord(Keys.ALT, Keys.DOWN))
+WebUI.sendKeys(findTestObject(null), Keys.chord(Keys.ALT, Keys.DOWN))
+WebUI.sendKeys(findTestObject(null), Keys.chord(Keys.ALT, Keys.DOWN))
+WebUI.click(findTestObject('Object Repository/Page_Daftar Sebagai Pemberi Dana  ALAMI Pee_40e437/input_Saya sudah membaca dan setuju dengan._c5cbb7'))
+
+WebUI.click(findTestObject('Object Repository/Page_Daftar Sebagai Pemberi Dana  ALAMI Pee_40e437/button_Lanjutkan_1'))
+
